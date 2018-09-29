3ee60b53568482f00ec5d4ddf5e03efc5ec57c84
Min Hee  Park <minheekangg@gmail.com>
2018-09-29 01:20:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheddar_cheese.find do |cheese|
-    cheese == cheese_types
+    cheese_types.include?(cheese)
   end
 end
